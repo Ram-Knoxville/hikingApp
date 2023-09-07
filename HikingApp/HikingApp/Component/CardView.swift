c12e3aebfe3ce66cd4caaf2b5f93fc6a8799d330
Ramiro Aguirre <ram@192.168.1.11>
2023-09-07 17:30:08 -0600

    added custom buttons and some styling for text

--- HikingApp/HikingApp/Component/CardView.swift
+++ HikingApp/HikingApp/Component/CardView.swift
@@ -13,15 +13,46 @@ struct CardView: View {
         
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [.customIndigoMedium, .customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    )
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                .scaledToFit()
+            VStack {
+                // MARK: - HEADER
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(.linearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium],
+                                    startPoint: .top,
+                                endPoint: .bottom))
+                        Spacer()
+                        Button {
+                            print("The button was pressed")
+                        }label: {
+                            CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [.customIndigoMedium, .customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
+                
+                // MARK: - FOOTER
             }
         } //: CARD
         .frame(width: 320, height: 570)
